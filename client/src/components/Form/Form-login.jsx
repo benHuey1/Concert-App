7c350557c1ea683f41b2edeea0edd73da5b7b7ac
Benja <benjaminmayeur01@gmail.com>
2023-08-30 20:24:57 +0200

    suite auth - cacher login-logout OK - logout sup session ok

--- client/src/components/Form/Form-login.jsx
+++ client/src/components/Form/Form-login.jsx
@@ -33,22 +33,27 @@ export default function FormLogin() {
         .catch( err => console.log(err))
     }, [])
 
-    const onSubmit = async () =>{
-        try {
-            const response = await axios.post("http://localhost:3001/login", {
+    const onSubmit = /*async*/ () =>{
+        /*try {*/
+            /*const response = await*/ axios.post("http://localhost:3001/login", {
                 mail,
                 password,
-            });
-            if (response.data.Login) {
-                console.log(response.data);
-                console.log("Welcome, " + response.data.name);
-                navigate('/');
-            } else {
-                alert("No record");
-            }
-        } catch (error) {
+            })
+            .then( res => {
+                if (res.data.login) {
+                    console.log(res.data);
+                    console.log("Welcome, " + res.data.name);
+                    navigate('/');
+                } else {
+                    console.log(res.data.message);
+                    alert(res.data.message);
+                }})
+                .catch(err => {
+                    console.log(err);
+                })
+        /*} catch (error) {
             console.log(error);
-        }
+        }*/
 };
 
     return (
@@ -80,7 +85,7 @@ export default function FormLogin() {
                 {...register("password", {
                 required: true,
                 minLength: 8,
-                maxLength: 20,
+                maxLength: 51,
                 pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
 
                 })} onChange={(e) => setPassword(e.target.value)}
@@ -92,7 +97,7 @@ export default function FormLogin() {
                 <p>Password cannot subceed 8 characters</p>
             )}
             {errors?.password?.type === "maxLength" && (
-                <p>Password cannot exceed 20 characters</p>
+                <p>Password cannot exceed 51 characters</p>
             )}
             {errors?.password?.type === "pattern" && (
                 // eslint-disable-next-line react/no-unescaped-entities
